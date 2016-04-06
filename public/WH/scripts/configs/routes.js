e324f551fe669b4e1ff445da2ae16b60ff57b13f
Ebad Nabi Hashmi <ibad.hashmi@yahoo.com>
2016-04-06 15:29:24 +0500

    chnges

--- public/WH/scripts/configs/routes.js
+++ public/WH/scripts/configs/routes.js
@@ -3,6 +3,12 @@ angular.module('procom').config(function ($routeProvider, $locationProvider) {
     $routeProvider
         .when('/account', {
             templateUrl: '/WH/views/account/profile.html'
+        })
+        .when('/account/managers', {
+            templateUrl: '/WH/views/account/profile.html'
+        })
+        .when('/account/managers/addItem', {
+            templateUrl: '/WH/views/account/addItem.html'
         });
 
     $locationProvider.html5Mode({
--- routes/account.js
+++ routes/account.js
@@ -10,10 +10,11 @@ function render(req,res){
         title: 'profile'
     });
 }
-
 router.get('/',render);
 router.get('/profile', render);
 router.get('/shop', render);
 router.get('/masterpiece',render);
+router.get('/managers',render);
+router.get('/managers/addItem',render);
 
 module.exports = router;
